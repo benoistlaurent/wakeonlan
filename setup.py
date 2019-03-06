a6e608e8500f41e0907d38aee2cbc9da076fb1fc
Benoist LAURENT <benoist.laurent@gmail.com>
2019-03-06 11:22:12 +0100

    create setup script & makefile

---
+++ setup.py
@@ -0,0 +1,38 @@
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst', 'rt') as f:
+    readme = f.read()
+
+with open('HISTORY.rst', 'rt') as f:
+    history = f.read()
+
+requirements = []
+
+setup(
+    author='Benoist LAURENT',
+    author_email='benoist.laurent@ibpc.fr',
+    name='wakeonlan',
+    description='A tool to wake machine from LAN',
+    long_description=readme + '\n\n' + history,
+    url='https://github.com/benoistlaurent/wakeonlan',
+    version='1.0.0',
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages=find_packages(include=['wakeonlan']),
+    entry_points={
+        'console_scripts': [
+            'wakeonlan=wakeonlan.cli:main',
+        ],
+    },
+)
